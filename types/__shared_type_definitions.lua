f753bffe95469d9c07f1f04d4c7f5e959b7d0555
IIYAMA <Jorikvanessen@hotmail.com>
2024-08-01 19:48:32 +0200

    add event onClientCoreCommand

--- types/__shared_type_definitions.lua
+++ types/__shared_type_definitions.lua
@@ -1375,3 +1375,5 @@ Use 3 to remove the paintjob.
 ---@alias monthdays 1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31
 ---@alias months 0|1|2|3|4|5|6|7|8|9|10|11
 ---@alias weekdays 0|1|2|3|4|5|6
+
+---@alias coreCommands "help"|"exit"|"quit"|"ver"|"time"|"showhud"|"binds"|"serial"|"connect"|"reconnect"|"bind"|"unbind"|"copygtacontrols"|"screenshot"|"saveconfig"
